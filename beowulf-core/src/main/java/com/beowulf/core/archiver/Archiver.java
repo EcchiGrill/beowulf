2a23c85b353dd21b18a43c5fcc7955f1bc1c284f
EcchiGrill <dmitry.oborsky@gmail.com>
2025-11-05 23:59:47 +0200

    :memo: Added javadoc

--- beowulf-core/src/main/java/com/beowulf/core/archiver/Archiver.java
+++ beowulf-core/src/main/java/com/beowulf/core/archiver/Archiver.java
@@ -4,7 +4,20 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public interface Archiver {
+    /**
+     * Compresses the given source directory or file into a single archive file.
+     *
+     * @param sourceDir path to file or directory to compress
+     * @param targetArchive path to resulting archive file (.zip, .tar, etc.)
+     */
     void compress(Path sourceDir, Path targetArchive) throws IOException;
+
+    /**
+     * Extracts the given archive file into the target directory.
+     *
+     * @param archive path to existing archive
+     * @param targetDir destination directory
+     */
     void decompress(Path archive, Path targetDir) throws IOException;
 
     String getName();
